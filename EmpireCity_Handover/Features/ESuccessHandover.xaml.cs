3252f44eb7cc210d06f54e9980a7479832c50561
thuanphan892 <thuan.phanthithanh@gmail.com>
2020-05-12 18:50:00 +0700

    Update new layouts

---
+++ EmpireCity_Handover/Features/ESuccessHandover.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EmpireCity_Handover.Features
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ESuccessHandover : ContentPage
+    {
+        public ESuccessHandover()
+        {
+            InitializeComponent();
+        }
+    }
+}
